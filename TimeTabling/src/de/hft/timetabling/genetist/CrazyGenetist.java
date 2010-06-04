666dfc26f250746ea8963fcb16975382dbbdaea5
steffenk.q@googlemail.com <steffenk.q@googlemail.com@09fc8f0f-b869-3d85-6b29-0e1737582408>
2010-06-04 17:31:52 +0000

    Improvement of the crazy genetist ...

--- TimeTabling/src/de/hft/timetabling/genetist/CrazyGenetist.java
+++ TimeTabling/src/de/hft/timetabling/genetist/CrazyGenetist.java
@@ -12,6 +12,7 @@ import de.hft.timetabling.common.IProblemInstance;
 import de.hft.timetabling.common.ISolution;
 import de.hft.timetabling.services.ICrazyGenetistService;
 import de.hft.timetabling.services.ISolutionTableService;
+import de.hft.timetabling.services.ServiceLocator;
 
 public class CrazyGenetist implements ICrazyGenetistService {
 
@@ -21,8 +22,12 @@ public class CrazyGenetist implements ICrazyGenetistService {
 
 	@Override
 	public ISolution recombineAndMutate() {
-		// TODO Please rewrite as a solution is no longer passed as a parameter.
-		setSolution(solution);
+
+		int noFeasibleRecombinationFoundExceptionCounter = 0;
+
+		ServiceLocator serviceLocator = ServiceLocator.getInstance();
+
+		setSolution(serviceLocator.getSolutionTableService());
 
 		ISolution back = null;
 
@@ -32,13 +37,14 @@ public class CrazyGenetist implements ICrazyGenetistService {
 			while ((otherSolution == null) || (bestSolution == null)
 					|| bestSolution.equals(otherSolution)) {
 				System.out.println("Trying to find solution to work with ...");
-				int n = (int) (solution.getActualSolutionTableCount() * Math
+				int n1 = (int) (solution.getActualSolutionTableCount() * Math
 						.random());
+				int n2 = (int) (solution.getActualSolutionTableCount() * Math
+						.random());
+				bestSolution = solution.getSolution(n1);
+				otherSolution = solution.getSolution(n2);
 
-				bestSolution = solution.getBestSolution();
-				otherSolution = solution.getSolution(n);
-
-				System.out.println("... found " + n + " ...");
+				System.out.println("... found " + n1 + ", " + n2 + " ...");
 			}
 			System.out.println(" ... take it!");
 			if (Math.random() < probability) {
@@ -47,6 +53,9 @@ public class CrazyGenetist implements ICrazyGenetistService {
 					back = mutateRoomStability(recombindation1(bestSolution,
 							otherSolution));
 				} catch (NoFeasibleRecombinationFoundException e) {
+					System.out
+							.println("[Caught NoFeasibleRecombinationFoundException]");
+					noFeasibleRecombinationFoundExceptionCounter++;
 					back = mutateRoomStability(recombindation2(bestSolution,
 							otherSolution));
 				}
@@ -58,7 +67,11 @@ public class CrazyGenetist implements ICrazyGenetistService {
 
 			bestSolution.increaseRecombinationCount();
 			otherSolution.increaseRecombinationCount();
+
 		}
+		System.out.println("Crazy Genetist is done. (Created "
+				+ noFeasibleRecombinationFoundExceptionCounter
+				+ " non feasible Solution with recombination1)");
 		return back;
 
 	}
@@ -81,14 +94,10 @@ public class CrazyGenetist implements ICrazyGenetistService {
 		while (myCurriculum == null) {
 			roomY = (int) (pi.getRooms().size() * Math.random());
 			periodX = (int) (pi.getNumberOfPeriods() * Math.random());
-			System.out.println("... trying to find curriculum [" + periodX
-					+ "][" + roomY + "] ...");
+			// System.out.println("... trying to find curriculum [" + periodX+
+			// "][" + roomY + "] ...");
 			if (courses[periodX][roomY] != null) {
 				Set<ICurriculum> cur = courses[periodX][roomY].getCurricula();
-
-				for (ICurriculum asd : cur) {
-					System.out.println("### " + asd.getId());
-				}
 
 				String curriculumNr = String.format("q%03d",
 						(int) (cur.size() * Math.random()));
@@ -99,21 +108,15 @@ public class CrazyGenetist implements ICrazyGenetistService {
 
 		System.out.println("... mutate ...");
 		for (int i = 0; i < courses.length; i++) {
-			System.out.println("#######1");
 			if (i != periodX) {
-				System.out.println("#######2");
 				for (int j = 0; j < courses[i].length; j++) {
-					System.out.println("#######3");
 					ICourse selectedCourse = courses[i][j];
 					if (selectedCourse != null) {
-						System.out.println("#######4");
 						Set<ICurriculum> tmpCur = selectedCourse.getCurricula();
 
 						for (ICurriculum iCurriculum : tmpCur) {
-							System.out.println("#######5");
 							if (iCurriculum.getId()
 									.equals(myCurriculum.getId())) {
-								System.out.println("#######6");
 								courses[i][j] = courses[i][roomY];
 								courses[i][roomY] = selectedCourse;
 								// No check neccessary because we only changed
@@ -133,8 +136,7 @@ public class CrazyGenetist implements ICrazyGenetistService {
 		return newSolution;
 	}
 
-	private ISolution recombindation1(ISolution bestSolution,
-			ISolution otherSolution)
+	private ISolution recombindation1(ISolution solution1, ISolution solution2)
 			throws NoFeasibleRecombinationFoundException {
 		System.out.println("Start recombnination 1 process ...");
 		ValidatorImpl vi = new ValidatorImpl();
@@ -144,17 +146,16 @@ public class CrazyGenetist implements ICrazyGenetistService {
 
 		do {
 
-			newSolution = bestSolution.clone();
-			System.out.println(newSolution.toString());
+			newSolution = solution1.clone();
 			IProblemInstance pi1 = newSolution.getProblemInstance();
 			int periodX1 = (int) (pi1.getNumberOfPeriods() * Math.random());
 
-			IProblemInstance pi2 = otherSolution.getProblemInstance();
+			IProblemInstance pi2 = solution2.getProblemInstance();
 			int periodX2 = (int) (pi2.getNumberOfPeriods() * Math.random());
 
 			ICourse[] oldCourses = newSolution.getCoding()[periodX1];
-			ICourse[] newCourses = otherSolution.getCoding()[periodX2];
-			newSolution.getCoding()[periodX1] = otherSolution.getCoding()[periodX2];
+			ICourse[] newCourses = solution2.getCoding()[periodX2];
+			newSolution.getCoding()[periodX1] = solution2.getCoding()[periodX2];
 			Set<ICourse> oldCourseSet = getCourseSet(oldCourses);
 			Set<ICourse> newCourseSet = getCourseSet(newCourses);
 
@@ -187,23 +188,13 @@ public class CrazyGenetist implements ICrazyGenetistService {
 				}
 			}
 
-			if (iterationCount > 1000) {
+			if (iterationCount++ > 100) {
 				throw new NoFeasibleRecombinationFoundException(
 						"Recombination 1 was not successful.");
 			}
 
-			if (iterationCount > 100) {
-				throw new NoFeasibleRecombinationFoundException(
-						"Recombination 1 was not successful.");
-			}
-
-			System.out.println("... recombindation1: did " + iterationCount++
-					+ " iteration ...");
-
-			if (checkForNullCourse(newSolution)) {
-				System.out.println("only null available");
-				System.exit(0);
-			}
+			// System.out.println("... recombindation1: did " + iterationCount +
+			// " iteration ...");
 
 		} while (!vi.isValidSolution(newSolution));
 		System.out.println("... done with recombination 1 process.");
@@ -219,27 +210,26 @@ public class CrazyGenetist implements ICrazyGenetistService {
 		return back;
 	}
 
-	private ISolution recombindation2(ISolution bestSolution,
-			ISolution otherSolution) {
+	private ISolution recombindation2(ISolution solution1, ISolution solution2) {
 		System.out.println("Start recombnination 2 process ...");
 		ICourse[][] oldBestSolution;
 		ValidatorImpl vi = new ValidatorImpl();
 		Set<ICourse> savingList = new HashSet<ICourse>();
-		for (int i = 0; i < bestSolution.getCoding().length; i++) {
-			for (int j = 0; j < bestSolution.getCoding()[i].length; j++) {
-				System.out.println("... course[" + i + "][" + j + "]");
-				if (bestSolution.getCoding()[i][j] == null) {
-					oldBestSolution = bestSolution.getCoding();
-					if (otherSolution.getCoding()[i][j] != null) {
-						bestSolution.getCoding()[i][j] = otherSolution
-								.getCoding()[i][j];
-						if (!vi.isValidSolution(bestSolution)) {
-							bestSolution.getCoding()[i][j] = oldBestSolution[i][j];
+		ISolution newSolution = solution1.clone();
+		for (int i = 0; i < newSolution.getCoding().length; i++) {
+			for (int j = 0; j < newSolution.getCoding()[i].length; j++) {
+				// System.out.println("... course[" + i + "][" + j + "] ...");
+				if (newSolution.getCoding()[i][j] == null) {
+					oldBestSolution = newSolution.getCoding();
+					if (solution2.getCoding()[i][j] != null) {
+						newSolution.getCoding()[i][j] = solution2.getCoding()[i][j];
+						if (!vi.isValidSolution(newSolution)) {
+							newSolution.getCoding()[i][j] = oldBestSolution[i][j];
 						} else {
 							Map<ICourse, CoursePosition> courseCountNew = countLectures(
-									bestSolution.getCoding(), otherSolution
+									newSolution.getCoding(), solution2
 											.getCoding()[i][j].getId());
-							int expectedSize = otherSolution.getCoding()[i][j]
+							int expectedSize = solution2.getCoding()[i][j]
 									.getNumberOfLectures();
 							if (courseCountNew.size() >= expectedSize) {// Gab
 								// and
@@ -255,9 +245,9 @@ public class CrazyGenetist implements ICrazyGenetistService {
 									if ((tmpX != i) && (tmpY != j)) {
 
 										savingList
-												.add(bestSolution.getCoding()[tmpX][tmpY]);
-										bestSolution.getCoding()[tmpX][tmpY] = null;
-										otherSolution.getCoding()[i][j] = null;
+												.add(newSolution.getCoding()[tmpX][tmpY]);
+										newSolution.getCoding()[tmpX][tmpY] = null;
+										solution2.getCoding()[i][j] = null;
 									}
 								}
 							}
@@ -267,11 +257,11 @@ public class CrazyGenetist implements ICrazyGenetistService {
 			}
 		}
 		System.out.println("... done with recombindation 2 process. ("
-				+ checkForNullCourse(bestSolution) + ")");
-		if (checkForNullCourse(bestSolution)) {
+				+ !checkForNullCourse(newSolution) + ")");
+		if (checkForNullCourse(newSolution)) {
 			System.out.println("ALL NULL");
 		}
-		return bestSolution;
+		return newSolution;
 	}
 
 	private Map<ICourse, CoursePosition> countLectures(ICourse[][] course,
